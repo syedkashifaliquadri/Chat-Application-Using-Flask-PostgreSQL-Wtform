a1880057c52a090669d7add61934babbd43a8cd3
mehmoodkashif09@gmail.com <mehmoodkashif09@gmail.com>
2019-11-14 18:04:48 +0500

    Add macro to index.html

--- wtform_fields.py
+++ wtform_fields.py
@@ -7,10 +7,13 @@ from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
 class RegistrationForm(FlaskForm):
     ''' Registration Form '''
 
-    username = StringField('username_label', validators=[InputRequired(message="Username required"), Length(min=4, max=25, message="Username must be between 4 and 25 character")])
+    username = StringField('username_label', validators=[InputRequired(message="Username required"), 
+                          Length(min=4, max=25, message="Username must be between 4 and 25 character")])
 
-    password = PasswordField('password_label', validators=[InputRequired(message="Password required"), Length(min=4, max=25, message="Password must be between 4 and 25 character")])
+    password = PasswordField('password_label', validators=[InputRequired(message="Password required"), 
+                            Length(min=4, max=25, message="Password must be between 4 and 25 character")])
 
-    confirm_pswd = PasswordField('confirm_pswd_label', validators=[InputRequired(message="Password required"), EqualTo('password', message='Password must match')])
+    confirm_pswd = PasswordField('confirm_pswd_label', validators=[InputRequired(message="Password required"), 
+                                EqualTo('password', message='Password must match')])
 
     submit_button = SubmitField('Create')
